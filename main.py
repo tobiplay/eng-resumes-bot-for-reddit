000003f95e7549f0ba3715383d102df78e7c1fbf
tobiplay <tobiplay@fa-meier.de>
2021-09-27 18:33:26 +0200

    Clean main.py and prepare for deploy to Heroku

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 from logging import raiseExceptions
 import praw
+import os
 import secret
 
 def main():
     try:
+        # Dev environment on local machine
+        # reddit = praw.Reddit(
+        #     client_id=secret.client_id,
+        #     client_secret=secret.client_secret,
+        #     user_agent=secret.user_agent,
+        #     username=secret.username,
+        #     password=secret.password
+        # )
+
         reddit = praw.Reddit(
-            client_id=secret.client_id,
-            client_secret=secret.client_secret,
-            user_agent=secret.user_agent,
-            username=secret.username,
-            password=secret.password
+            client_id=os.environ[CLIENT_ID],
+            client_secret=os.environ[CLIENT_SECRET],
+            user_agent=os.environ[USER_AGENT],
+            username=os.environ[USERNAME],
+            password=os.environ[PASSWORD]
         )
+
     except:
         raise ConnectionError("Network connection could not be established")
 
     # The standard bot_message as a reply to new posts
-    bot_message = '''Hi there! Thanks for posting to r/EngineeringResumes. If you haven't already, make sure to check out these posts and edit your resume accordingly:
-* [Wiki](https://www.reddit.com/r/EngineeringResumes/comments/m2cc65/new_and_improved_wiki/)
-* [Resume critique videos](https://www.reddit.com/r/EngineeringResumes/comments/j0ujid/resume_critique_videos_5_6/)
-* [Resume redline albums](https://www.reddit.com/r/EngineeringResumes/comments/p5y5at/resume_redline_imgur_albums/)
-* [Learn how to apply the STAR method](https://www.careereducation.columbia.edu/resources/resumes-impact-creating-strong-bullet-points)
-* [Learn how to apply the XYZ method](https://www.inc.com/bill-murphy-jr/google-recruiters-say-these-5-resume-tips-including-x-y-z-formula-will-improve-your-odds-of-getting-hired-at-google.html) \n
+    bot_message = '''Hi there! Thanks for posting to r/EngineeringResumes. If you haven't already, make sure to check out these posts and edit your resume accordingly: \n
+- [Wiki](https://www.reddit.com/r/EngineeringResumes/comments/m2cc65/new_and_improved_wiki/) \n
+- [Resume critique videos](https://www.reddit.com/r/EngineeringResumes/comments/j0ujid/resume_critique_videos_5_6/) \n
+- [Resume redline albums](https://www.reddit.com/r/EngineeringResumes/comments/p5y5at/resume_redline_imgur_albums/) \n
+- [Learn how to apply the STAR method](https://www.careereducation.columbia.edu/resources/resumes-impact-creating-strong-bullet-points) \n
+- [Learn how to apply the XYZ method](https://www.inc.com/bill-murphy-jr/google-recruiters-say-these-5-resume-tips-including-x-y-z-formula-will-improve-your-odds-of-getting-hired-at-google.html) \n
 *Beep, boop - this is an automated reply. If you've got any questions surrounding my existance, please [contact the moderators of this subreddit](https://www.reddit.com/message/compose/?to=/r/engineeringresumes&subject=Problem%20or%20question%20regarding%20bot&message=)!*
 '''
 
-    submission_limit = 10
+    submission_limit = 2
+
     if submission_limit > 10:
-        print("Submission limit is too high at {} and automatically set to 1".format(submission_limit))
+        print("Submission limit is too high at {} and therefore automatically set to 5".format(submission_limit))
         submission_limit = 10
 
     # Are we able to only read but not edit comments? -> False, if we can edit them, too
@@ -34,24 +46,23 @@ def main():
 
     subreddit = reddit.subreddit("engineeringresumes")
     # print(subreddit.display_name, subreddit.title)
-    # print(bot_message)
 
     # Assume you have a Subreddit instance bound to variable `subreddit`
     for submission in subreddit.new(limit=submission_limit):
-        # Print information about each submission in the subreddit up until the set limit
+        # We'll go through each submission in the subreddit up until the set limit
         # print(submission.title, submission.score, submission.url, submission.id)
 
         # State for if the bot has already replied to the submission
-        # Necessary, because Heroku Dynos don't allow persistance
+        # Necessary, because Heroku Dynos don't allow persistance for any kind of data
         already_answered = False
-        print("Currently checking submission " + str(submission.id) + " with the title '" + str(submission.title)[:15] + "...'")
+        print("Currently checking submission {} with the title '{}...'").format(str(submission.id), str(submission.title)[:15])
 
         while not already_answered: 
 
             # The bot will only make top-level replies to a post, so we only look for those top-level comments on each post
             for comment in submission.comments:
-                # Check if author of current top-level comment is our bot
-                if comment.author == "TobiPlay":
+                # Check if the author of current top-level comment is our bot
+                if comment.author == str(os.environ[USERNAME]):
                     # Was already answered -> exit loop and exit the routine as we don't need to check any more comments
                     already_answered = True
                     break
